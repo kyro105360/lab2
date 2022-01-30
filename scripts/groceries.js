10af2cf68d69544c0a7b0befb89a59351881bf3a
Andre <>
2022-01-30 17:24:16 -0500

    Added Both option for dietary restrictions (Vegetarian and Gluten-Free). Increased font size of headings and paragraphs.

--- scripts/groceries.js
+++ scripts/groceries.js
@@ -35,6 +35,9 @@ function restrictListProducts(prods, restriction) {
 		else if ((restriction == "GlutenFree") && (prods[i].glutenFree == true)){
 			product_names.push(prods[i].name);
 		}
+		else if ((restriction == "Both") && (prods[i].vegetarian == true) && (prods[i].glutenFree == true)){
+			product_names.push(prods[i].name);
+		}
 		else if (restriction == "None"){
 			product_names.push(prods[i].name);
 		}
